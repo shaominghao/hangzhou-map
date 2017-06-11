a62f2923c5b5c8bc52cb960749d85df23e940633
lihaoliang <hzlihaoliang@corp.netease.com>
2017-06-11 13:33:39 +0800

    modify data

--- index.js
+++ index.js
@@ -188,40 +188,40 @@ chart5.setOption({
     },
     calculable: true,
     legend: {
-        data: ['蒸发量', '平均温度']
+        data: ['数量', '质量']
     },
     xAxis: [
         {
             type: 'category',
-            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+            data: ['市公积金中心', '人力社保局', '公安局', '市场监管局', '地税局', '国税局', '市建委', '民政局', '国土局', '房管局']
         }
     ],
     yAxis: [
         {
             type: 'value',
-            name: '水量',
+            name: '数量',
             axisLabel: {
-                formatter: '{value} ml'
+                formatter: '{value}'
             }
         }, {
             type : 'value',
-            name : '温度',
+            name : '质量',
             axisLabel : {
-                formatter: '{value} °C'
+                formatter: '{value} %'
             }
         }
     ],
     series: [
         {
-            name: '蒸发量',
+            name: '数量',
             type: 'bar',
-            data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+            data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0]
         },
         {
-            name: '平均温度',
+            name: '质量',
             type: 'line',
             yAxisIndex: 1,
-            data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+            data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5]
         }
     ]
 

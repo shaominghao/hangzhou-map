5a9e4d96f2691e497e9ab266046c52df722d40aa
lihaoliang <hzlihaoliang@corp.netease.com>
2017-06-12 22:25:57 +0800

    add gulp watch compile less

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -2,18 +2,19 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
 
 // 编译less
 gulp.task('css', function () {
     gulp.src('../src/styles/index.less')
         .pipe(less())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie > 8']
-        }))
         .pipe(cleanCSS())
         .pipe(rename('index.css'))
         .pipe(gulp.dest('../dist/styles'));
 });
 
-gulp.task('default', ['css']);
+gulp.task('watch',function () {
+    gulp.watch('../src/styles/*.less',['css'])
+});
+
+gulp.task('default', ['watch']);
